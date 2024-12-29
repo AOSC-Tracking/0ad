AOSC-Tracking/0ad

commit b90280855f43ef42159dc689fcf59a0d1ec78ce2

Multiplayer saved games
Enables to save multiplayer games.
When the savegame is loaded, the settings are frozen (except the non-AI-player assignment settings).

binaries/data/mods/public/gui/gamesetup/Pages/GameSetupPage/GameSettings/PerPlayer/PlayerName.js
@@ -5,6 +5,7 @@ PlayerSettingControls.PlayerName = class PlayerName extends GameSettingControl
 	constructor(...args)
 	{
 		super(...args);
+		g_GameSettings.playerName.isSavedGame = this.isSavedGame;
 
 		this.playerName = Engine.GetGUIObjectByName("playerName[" + this.playerIndex + "]");
 		g_GameSettings.playerCount.watch(() => this.render(), ["nbPlayers"]);
@@ -29,8 +30,8 @@ PlayerSettingControls.PlayerName = class PlayerName extends GameSettingControl
 
 	render()
 	{
-		let name = this.guid ? g_PlayerAssignments[this.guid].name :
-			g_GameSettings.playerName.values[this.playerIndex];
+		let name = this.guid && this.isSavedGame ?
+			g_PlayerAssignments[this.guid].name : g_GameSettings.playerName.values[this.playerIndex];
 
 		if (g_IsNetworked)
 		{

binaries/data/mods/public/gamesettings/attributes/PlayerName.js
@@ -7,6 +7,8 @@
  */
 GameSettings.prototype.Attributes.PlayerName = class PlayerName extends GameSetting
 {
+	randomPicked = false;
+
 	init()
 	{
 		// NB: watchers aren't auto-triggered when modifying array elements.
@@ -21,6 +23,9 @@ GameSettings.prototype.Attributes.PlayerName = class PlayerName extends GameSett
 			attribs.settings.PlayerData = [];
 		while (attribs.settings.PlayerData.length < this.values.length)
 			attribs.settings.PlayerData.push({});
+		if (this.isSavedGame && !this.randomPicked)
+			return;
+
 		for (let i in this.values)
 			if (this.values[i])
 				attribs.settings.PlayerData[i].Name = this.values[i];
@@ -87,7 +92,7 @@ GameSettings.prototype.Attributes.PlayerName = class PlayerName extends GameSett
 			const names = this.settings.civData[civ].AINames;
 			const remainingNames = names.filter(name => !AIPlayerNamesList.includes(name));
 			const chosenName = pickRandom(remainingNames.length ? remainingNames : names);
-			
+
 			// Avoid translating AI names if the game is networked, so all players see and refer to
 			// English names instead of names in the language of the host.
 			const translatedCountLabel = this.settings.isNetworked ? this.CountLabel : translate(this.CountLabel);
@@ -98,7 +103,7 @@ GameSettings.prototype.Attributes.PlayerName = class PlayerName extends GameSett
 					count++;
 				return count;
 			}, 0);
-			
+
 			AIPlayerNamesList.push(chosenName);
 
 			this.values[i] = !duplicateNameCount ? translatedChosenName :
@@ -108,7 +113,10 @@ GameSettings.prototype.Attributes.PlayerName = class PlayerName extends GameSett
 				});
 		}
 		if (picked)
+		{
+			this.randomPicked = true;
 			this.trigger("values");
+		}
 		return picked;
 	}
 

binaries/data/mods/public/gamesettings/GameSettings.js
@@ -11,12 +11,16 @@
  */
 class GameSettings
 {
-	init(mapCache)
+	init(mapCache, savegameID)
 	{
 		if (!mapCache)
 			mapCache = new MapCache();
 		Object.defineProperty(this, "mapCache", {
 			"value": mapCache,
+		});
+
+		Object.defineProperty(this, "savegameID", {
+			"value": savegameID,
 		});
 
 		// Load all possible civ data - don't presume that some will be available.
@@ -39,6 +43,15 @@ class GameSettings
 		for (let comp in this)
 			if (this[comp].init)
 				this[comp].init();
+
+		if (!savegameID)
+			return this;
+
+		const initAttributes = Engine.LoadSavedGameMetadata(savegameID).initAttributes;
+
+		// Remove the gaia entry.
+		initAttributes.settings.PlayerData.splice(0, 1);
+		this.fromInitAttributes(initAttributes);
 
 		return this;
 	}
@@ -142,7 +155,7 @@ class GameSettings
 
 		// NB: for multiplayer support, the clients must be listening to "start" net messages.
 		if (this.isNetworked)
-			Engine.StartNetworkGame(this.finalizedAttributes, storeReplay);
+			Engine.StartNetworkGame(this.savegameID, this.finalizedAttributes, storeReplay);
 		else
 			Engine.StartGame(this.finalizedAttributes, playerAssignments.local.player, storeReplay);
 	}

binaries/data/mods/public/gui/gamesetup/Controllers/GameSettingsController.js
@@ -3,7 +3,7 @@
  */
 class GameSettingsController
 {
-	constructor(setupWindow, netMessages, playerAssignmentsController, mapCache)
+	constructor(setupWindow, netMessages, playerAssignmentsController, mapCache, isSavedGame)
 	{
 		this.setupWindow = setupWindow;
 		this.mapCache = mapCache;
@@ -25,7 +25,7 @@ class GameSettingsController
 		this.loadingChangeHandlers = new Set();
 		this.settingsLoadedHandlers = new Set();
 
-		setupWindow.registerLoadHandler(this.onLoad.bind(this));
+		setupWindow.registerLoadHandler(this.onLoad.bind(this, isSavedGame));
 		setupWindow.registerGetHotloadDataHandler(this.onGetHotloadData.bind(this));
 
 		setupWindow.registerClosePageHandler(this.onClose.bind(this));
@@ -75,29 +75,32 @@ class GameSettingsController
 		this.settingsLoadedHandlers.add(handler);
 	}
 
-	onLoad(initData, hotloadData)
+	onLoad(isSavedGame, initData, hotloadData)
 	{
-		// This initial settings parsing in wrapped in a try-catch because it can fail unexpectedly,
-		// and particularly could fail with mods that change persistent settings, so this is
-		// difficult to fully fix from the gameSettings code.
-		// Also include hotloaded data because that can also fail and having to restart isn't very useful.
-		try {
-			if (hotloadData)
-				this.parseSettings(hotloadData.initAttributes);
-			else if (g_IsController && (initData?.gameSettings || this.persistentMatchSettings.enabled))
-			{
-				// Allow opting-in to persistence when sending initial data (though default off)
-				if (initData?.gameSettings)
-					this.persistentMatchSettings.enabled = !!initData.gameSettings?.usePersistence;
-				const settings = initData?.gameSettings || this.persistentMatchSettings.loadFile();
-					if (settings)
-						this.parseSettings(settings);
+		if (!isSavedGame)
+		{
+			// This initial settings parsing in wrapped in a try-catch because it can fail unexpectedly,
+			// and particularly could fail with mods that change persistent settings, so this is
+			// difficult to fully fix from the gameSettings code.
+			// Also include hotloaded data because that can also fail and having to restart isn't very useful.
+			try {
+				if (hotloadData)
+					this.parseSettings(hotloadData.initAttributes);
+				else if (g_IsController && (initData?.gameSettings || this.persistentMatchSettings.enabled))
+				{
+					// Allow opting-in to persistence when sending initial data (though default off)
+					if (initData?.gameSettings)
+						this.persistentMatchSettings.enabled = !!initData.gameSettings?.usePersistence;
+					const settings = initData?.gameSettings || this.persistentMatchSettings.loadFile();
+						if (settings)
+							this.parseSettings(settings);
+				}
+			} catch(err) {
+				error("There was an error loading game settings. You may need to disable persistent match settings.");
+				warn(err?.toString() ?? uneval(err));
+				if (err.stack)
+					warn(err.stack)
 			}
-		} catch(err) {
-			error("There was an error loading game settings. You may need to disable persistent match settings.");
-			warn(err?.toString() ?? uneval(err));
-			if (err.stack)
-				warn(err.stack)
 		}
 
 		// If the new settings led to AI & players conflict, remove the AI.

binaries/data/mods/public/gui/gamesetup/Controllers/PlayerAssignmentsController.js
@@ -3,7 +3,7 @@
  */
 class PlayerAssignmentsController
 {
-	constructor(setupWindow, netMessages)
+	constructor(setupWindow, netMessages, isSavedGame)
 	{
 		this.clientJoinHandlers = new Set();
 		this.clientLeaveHandlers = new Set();
@@ -35,7 +35,7 @@ class PlayerAssignmentsController
 		setupWindow.registerGetHotloadDataHandler(this.onGetHotloadData.bind(this));
 		netMessages.registerNetMessageHandler("players", this.onPlayerAssignmentMessage.bind(this));
 
-		this.registerClientJoinHandler(this.onClientJoin.bind(this));
+		this.registerClientJoinHandler(this.onClientJoin.bind(this, isSavedGame));
 	}
 
 	registerPlayerAssignmentsChangeHandler(handler)
@@ -93,10 +93,12 @@ class PlayerAssignmentsController
 	 * On client join, try to assign them to a free slot.
 	 * (This is called before g_PlayerAssignments is updated).
 	 */
-	onClientJoin(newGUID, newAssignments)
+	onClientJoin(isSavedGame, newGUID, newAssignments)
 	{
-		if (!g_IsController || newAssignments[newGUID].player != -1)
+		if (!g_IsController || newAssignments[newGUID].player !== -1 || isSavedGame)
+		{
 			return;
+		}
 
 		// Assign the client (or only buddies if prefered) to a free slot
 		if (newGUID != Engine.GetPlayerGUID())

binaries/data/mods/public/gui/gamesetup/Pages/AIConfigPage/AIConfigPage.js
@@ -7,7 +7,7 @@ class AIGameSettingControls
 
 SetupWindowPages.AIConfigPage = class
 {
-	constructor(setupWindow)
+	constructor(setupWindow, isSavedGame)
 	{
 		this.gameSettingsController = setupWindow.controls.gameSettingsController;
 
@@ -18,7 +18,8 @@ SetupWindowPages.AIConfigPage = class
 
 		for (let name of this.AIGameSettingControlOrder)
 			this.AIGameSettingControls[name] =
-				new AIGameSettingControls[name](this, undefined, undefined, setupWindow);
+				new AIGameSettingControls[name](this, undefined, undefined, setupWindow,
+					isSavedGame);
 
 		this.aiDescription = new AIDescription(this, setupWindow);
 
@@ -38,12 +39,12 @@ SetupWindowPages.AIConfigPage = class
 		return this.row++;
 	}
 
-	openPage(playerIndex)
+	openPage(playerIndex, enabled)
 	{
 		this.playerIndex = playerIndex;
 
 		for (let handler of this.openPageHandlers)
-			handler(playerIndex);
+			handler(playerIndex, enabled);
 
 		this.aiConfigPage.hidden = false;
 	}

binaries/data/mods/public/gui/gamesetup/Pages/AIConfigPage/AIGameSettingControl.js
@@ -1,8 +1,8 @@
 class AIGameSettingControlDropdown extends GameSettingControlDropdown
 {
-	onOpenPage(playerIndex)
+	onOpenPage(playerIndex, enabled)
 	{
-		this.setEnabled(true);
+		this.setEnabled(enabled);
 		this.playerIndex = playerIndex;
 		this.render();
 	}

binaries/data/mods/public/gui/gamesetup/Pages/GameSetupPage/GameSettings/GameSettingControl.js
@@ -13,12 +13,13 @@
  */
 class GameSettingControl /* extends Profilable /* Uncomment to profile controls without hassle. */
 {
-	constructor(gameSettingControlManager, category, playerIndex, setupWindow)
+	constructor(gameSettingControlManager, category, playerIndex, setupWindow, isSavedGame)
 	{
 		// Store arguments
 		{
 			this.category = category;
 			this.playerIndex = playerIndex;
+			this.isSavedGame = isSavedGame;
 
 			this.setupWindow = setupWindow;
 			this.gameSettingsController = setupWindow.controls.gameSettingsController;
@@ -55,6 +56,9 @@ class GameSettingControl /* extends Profilable /* Uncomment to profile controls 
 
 		if (this.onPlayerAssignmentsChange)
 			this.playerAssignmentsController.registerPlayerAssignmentsChangeHandler(this.onPlayerAssignmentsChange.bind(this));
+
+		if (isSavedGame)
+			this.setEnabled(this.EnabledWhenSavedGame);
 	}
 
 	setTitle(titleCaption)
@@ -79,7 +83,7 @@ class GameSettingControl /* extends Profilable /* Uncomment to profile controls 
 
 	setEnabled(enabled)
 	{
-		this.enabled = enabled;
+		this.enabled = enabled && (!this.isSavedGame || this.EnabledWhenSavedGame);
 		this.updateVisibility();
 	}
 
@@ -137,6 +141,8 @@ class GameSettingControl /* extends Profilable /* Uncomment to profile controls 
 				(autocomplete[this.AutocompleteOrder] || []).concat(newEntries);
 	}
 }
+
+GameSettingControl.prototype.EnabledWhenSavedGame = false;
 
 GameSettingControl.prototype.TitleCaptionFormat =
 	translateWithContext("Title for specific setting", "%(setting)s:");

commit d064cd9e0607633af5df427c819d4a03f6d4f9d9

Never change the names in the "Player Name" column
The intent of this branch was to not change the names when a saved game
is loaded. So that one can see who was where in the saved game.
It was implemented wrong: The names didn't change when no savegame was
loaded and changed when a saved game was loaded.

This commit make the soved game case and no saved game case consistent.

Defect introduced in b90280855f.

binaries/data/mods/public/gui/gamesetup/Pages/GameSetupPage/GameSettings/PerPlayer/PlayerName.js
@@ -8,6 +8,7 @@ PlayerSettingControls.PlayerName = class PlayerName extends GameSettingControl
 
 		this.playerName = Engine.GetGUIObjectByName("playerName[" + this.playerIndex + "]");
 		g_GameSettings.playerCount.watch(() => this.render(), ["nbPlayers"]);
+		g_GameSettings.playerName.watch(this.render.bind(this), ["values"]);
 
 		this.guid = undefined;
 		this.render();
@@ -29,15 +30,14 @@ PlayerSettingControls.PlayerName = class PlayerName extends GameSettingControl
 
 	render()
 	{
-		let name = this.guid && this.isSavedGame ?
-			g_PlayerAssignments[this.guid].name : g_GameSettings.playerName.values[this.playerIndex];
+		let name = g_GameSettings.playerName.values[this.playerIndex];
 
 		if (g_IsNetworked)
 		{
 			let status = this.guid ? g_PlayerAssignments[this.guid].status : this.ReadyTags.length - 1;
 			name = setStringTags(name, this.ReadyTags[status]);
 		}
-		else if (name && !this.guid)
+		else if (name)
 		{
 			name = translate(name);
 		}