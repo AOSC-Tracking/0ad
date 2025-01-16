MaviLabArt/0ad-android

commit dcb8ef9b52d2cc369353245027a9c264bc590bf4

Don't show warning on ai -> ai messages
AI's can't receive chat messages. AI's sometimes send chat messages to
other AI's.

Defect introduced in e04506814a.
Refs: #7466

(cherry picked from commit fde696a401ea08c7291ed4f3ee8fea0b8dd7ed55)
Signed-off-by: Itms <itms@wildfiregames.com>

binaries/data/mods/public/gui/session/chat/ChatMessageFormatPlayer.js
@@ -93,7 +93,7 @@ class ChatMessageFormatPlayer
 		{
 			addresseeGUID = this.matchUsername(msg.text);
 			let addressee = g_PlayerAssignments[addresseeGUID];
-			if (!addressee)
+			if (msg.player === undefined && !addressee)
 			{
 				warn("Couldn't find chat message receiver: " + msg.text);
 				return false;

AOSC-Tracking/0ad

commit 604a53a0a1455dac165db0275f26e0f2997edeb9

Always early return if no receiver is found
This lead to an error because `addressee` is `undefined`.

Defect introduced in dcb8ef9b52.
Fixes #7466
Reported-by: @Langbart

binaries/data/mods/public/gui/session/chat/ChatMessageFormatPlayer.js
@@ -93,9 +93,10 @@ class ChatMessageFormatPlayer
 		{
 			addresseeGUID = this.matchUsername(msg.text);
 			let addressee = g_PlayerAssignments[addresseeGUID];
-			if (msg.player === undefined && !addressee)
+			if (!addressee)
 			{
-				warn("Couldn't find chat message receiver: " + msg.text);
+				if (msg.player === undefined)
+					warn("Couldn't find chat message receiver: " + msg.text);
 				return false;
 			}
 