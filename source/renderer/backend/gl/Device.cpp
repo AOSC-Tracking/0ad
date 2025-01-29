1c72baca494c12822766dcfabb9f775154709146
phosit <phosit@autistici.org>
2025-01-29 12:47:21 +0100

    Remove FALLTHROUGH on empty cases
    `#include "lib/code_annotation.h"` can be removed in some places.

--- source/renderer/backend/gl/Device.cpp
+++ source/renderer/backend/gl/Device.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2024 Wildfire Games.
+/* Copyright (C) 2025 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -1008,21 +1008,21 @@ bool CDevice::IsTextureFormatSupported(const Format format) const
 	case Format::UNDEFINED:
 		break;
 
-	case Format::R8G8B8_UNORM: FALLTHROUGH;
-	case Format::R8G8B8A8_UNORM: FALLTHROUGH;
-	case Format::A8_UNORM: FALLTHROUGH;
+	case Format::R8G8B8_UNORM:
+	case Format::R8G8B8A8_UNORM:
+	case Format::A8_UNORM:
 	case Format::L8_UNORM:
 		supported = true;
 		break;
 
-	case Format::R32_SFLOAT: FALLTHROUGH;
-	case Format::R32G32_SFLOAT: FALLTHROUGH;
-	case Format::R32G32B32_SFLOAT: FALLTHROUGH;
+	case Format::R32_SFLOAT:
+	case Format::R32G32_SFLOAT:
+	case Format::R32G32B32_SFLOAT:
 	case Format::R32G32B32A32_SFLOAT:
 		break;
 
-	case Format::D16_UNORM: FALLTHROUGH;
-	case Format::D24_UNORM: FALLTHROUGH;
+	case Format::D16_UNORM:
+	case Format::D24_UNORM:
 	case Format::D32_SFLOAT:
 		supported = true;
 		break;
@@ -1035,9 +1035,9 @@ bool CDevice::IsTextureFormatSupported(const Format format) const
 	case Format::D32_SFLOAT_S8_UINT:
 		break;
 
-	case Format::BC1_RGB_UNORM: FALLTHROUGH;
-	case Format::BC1_RGBA_UNORM: FALLTHROUGH;
-	case Format::BC2_UNORM: FALLTHROUGH;
+	case Format::BC1_RGB_UNORM:
+	case Format::BC1_RGBA_UNORM:
+	case Format::BC2_UNORM:
 	case Format::BC3_UNORM:
 		supported = m_Capabilities.S3TC;
 		break;
