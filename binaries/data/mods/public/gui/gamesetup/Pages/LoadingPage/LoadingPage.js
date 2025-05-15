f7aed0393afcbdcef23a8bdbe1f27b4d6effc8e7
Kate <curvature@giteauser>
2025-05-16 00:39:44 +0200

    Update binaries/data/mods/public/gui/gamesetup/Pages/LoadingPage/LoadingPage.js

--- binaries/data/mods/public/gui/gamesetup/Pages/LoadingPage/LoadingPage.js
+++ binaries/data/mods/public/gui/gamesetup/Pages/LoadingPage/LoadingPage.js
@@ -7,11 +7,23 @@ SetupWindowPages.LoadingPage = class
 {
 	constructor(setupWindow)
 	{
+		// Add a button to return here.
+		Engine.GetGUIObjectByName("returnButton").onPress = this.closePage.bind(this);
+
 		setupWindow.controls.gameSettingsController.registerLoadingChangeHandler((loading) => this.onLoadingChange(loading));
+
+
 	}
 
 	onLoadingChange(loading)
 	{
 		Engine.GetGUIObjectByName("loadingPage").hidden = !loading;
+	}
+
+	closePage()
+	{
+		//If the user cancels the join attempt, we close this page
+		Engine.GetGUIObjectByName("loadingPage").hidden = true;
+		Engine.SwitchGuiPage("page_lobby.xml", { "dialog": false });
 	}
 };
