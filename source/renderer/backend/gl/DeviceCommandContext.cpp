008ed33e8b0c91cce6013c365f2bb45905331b09
phosit <phosit@autistici.org>
2025-05-21 13:59:16 +0200

    Remove wrong UNUSED2
    Some variable are marked unused even though they are used.

--- source/renderer/backend/gl/DeviceCommandContext.cpp
+++ source/renderer/backend/gl/DeviceCommandContext.cpp
@@ -899,8 +899,6 @@ void CDeviceCommandContext::ResolveFramebuffer(
 	ENSURE(destinationFramebuffer->GetWidth() == sourceFramebuffer->GetWidth());
 	ENSURE(destinationFramebuffer->GetHeight() == sourceFramebuffer->GetHeight());
 #if CONFIG2_GLES
-	UNUSED2(destinationFramebuffer);
-	UNUSED2(sourceFramebuffer);
 	debug_warn("CDeviceCommandContext::ResolveFramebuffer is not implemented for GLES");
 #else
 	// Source framebuffer should not be backbuffer.
@@ -1200,8 +1198,6 @@ void CDeviceCommandContext::DrawInstanced(
 #if CONFIG2_GLES
 	ENSURE(!m_Device->GetCapabilities().instancing);
 	UNUSED2(firstVertex);
-	UNUSED2(vertexCount);
-	UNUSED2(instanceCount);
 #else
 	glDrawArraysInstancedARB(GL_TRIANGLES, firstVertex, vertexCount, instanceCount);
 #endif
@@ -1230,8 +1226,6 @@ void CDeviceCommandContext::DrawIndexedInstanced(
 	// in Mesa 7.10 swrast with index VBOs).
 #if CONFIG2_GLES
 	ENSURE(!m_Device->GetCapabilities().instancing);
-	UNUSED2(indexCount);
-	UNUSED2(firstIndex);
 	UNUSED2(instanceCount);
 #else
 	glDrawElementsInstancedARB(GL_TRIANGLES, indexCount, GL_UNSIGNED_SHORT,
