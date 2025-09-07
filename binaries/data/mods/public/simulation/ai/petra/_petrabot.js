3b55fc345c39cd5fa83fc2206be246647dd91677
phosit <phosit@autistici.org>
2025-09-07 16:09:20 +0200

    Remove this.isDeserialized from BaseAI
    `this.isDeserialized` is only required for AIs which `Deserialize`
    function depends on the game state.

--- binaries/data/mods/public/simulation/ai/petra/_petrabot.js
+++ binaries/data/mods/public/simulation/ai/petra/_petrabot.js
@@ -93,6 +93,9 @@ PetraBot.prototype.CustomInit = function(gameState)
 
 PetraBot.prototype.OnUpdate = function(sharedScript)
 {
+	if (this.isDeserialized)
+		this.Init(state, playerID, sharedAI);
+
 	if (this.gameFinished || this.gameState.playerData.state == "defeated")
 		return;
 
